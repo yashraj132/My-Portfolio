e94875b78dbff0331e5df474febe300fc664593e
yashraj132 <pandeyy304@gmail.com>
2024-07-20 11:45:54 +0530

    updated

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,6 +1,6 @@
-import Work1 from "../../assets/work-1.svg";
-import Work2 from "../../assets/work-2.svg";
-import Work3 from "../../assets/work-3.svg";
+import Work1 from "../../assets/gym back.jpg";
+import Work2 from "../../assets/hungry panda.jpg";
+import Work3 from "../../assets/chat app.png";
 // import Work4 from "../../assets/work-4.svg";
 // import Work5 from "../../assets/work-5.svg";
 // import Work6 from "../../assets/work-6.svg";
